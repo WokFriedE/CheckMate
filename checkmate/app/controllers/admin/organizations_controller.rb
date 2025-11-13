3f7456a5ab8790438a0c8be527bfaede3540cb0d
WokFriedE <ethankh717@gmail.com>
2025-11-13 01:37:28 -0500

    fix org create

--- checkmate/app/controllers/admin/organizations_controller.rb
+++ checkmate/app/controllers/admin/organizations_controller.rb
@@ -25,7 +25,7 @@ module Admin
     def organization_params
       # TODO: Adding password strength validation in the model
       # TODO: Hashing the password before storage (using has_secure_password or similar)
-      params.require(:organization).permit(:org_name, :org_location, :public_access, :parent_org_id, :prebook_timeframe)
+      params.require(:organization).permit(:org_name, :org_location, :public_access, :parent_org_id, :prebook_timeframe, :org_pwd)
     end
   end
 end
--- checkmate/app/models/organization.rb
+++ checkmate/app/models/organization.rb
@@ -15,14 +15,9 @@ class Organization < ApplicationRecord
 
   before_create :assign_unique_org_id
 
-  validates :org_id, presence: true, uniqueness: true, numericality: {
-    only_integer: true,
-    greater_than_or_equal_to: 0
-  }
-
   # Randomization prevents people from trying to iterate through orgs
   def assign_unique_org_id
-    max_retries = 100
+    max_retries = 10
     retries = 0
     loop do
       self.org_id = SecureRandom.random_number(1_000_000_000)
