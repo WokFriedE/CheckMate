64bb88ccd8f804cda1f506be876a0a6edd5c5c87
MinaPM <mina.messad10+github@gmail.com>
2025-11-21 23:40:59 -0500

    Implement checkout feature with controller, views, and tests; update navbar visibility logic and enhance UI components

--- checkmate/config/routes.rb
+++ checkmate/config/routes.rb
@@ -1,27 +1,28 @@
 Rails.application.routes.draw do
+  get 'checkout/index'
   resources :returns
   resources :orders
   resources :inventories
+  resources :checkout
 
-  root "application#index"
-  
-  get  "signup", to: "authentication#signup_form"
-  post "signup", to: "authentication#signup"
-  
-  get  "login", to: "authentication#login_form"
-  post "login", to: "authentication#login"
-  
-  delete "logout", to: "authentication#logout"
-  
-  get "landing", to: "landing#index"
-  
-  namespace :admin do 
-      resources :org_roles, only: [:index, :new, :create]
-      resources :organizations
+  root 'application#index'
+
+  get  'signup', to: 'authentication#signup_form'
+  post 'signup', to: 'authentication#signup'
+
+  get  'login', to: 'authentication#login_form'
+  post 'login', to: 'authentication#login'
+
+  delete 'logout', to: 'authentication#logout'
+
+  get 'landing', to: 'landing#index'
+
+  namespace :admin do
+    resources :org_roles, only: %i[index new create]
+    resources :organizations
   end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
+  get 'up' => 'rails/health#show', as: :rails_health_check
 end
