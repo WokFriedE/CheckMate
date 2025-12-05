afee28649444ca2215c9badf1f1cd995ba8cbb94
WokFriedE <ethankh717@gmail.com>
2025-12-05 02:52:49 -0500

    fix test

--- checkmate/test/controllers/checkout_controller_test.rb
+++ checkmate/test/controllers/checkout_controller_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class CheckoutControllerTest < ActionDispatch::IntegrationTest
-  test 'should get index' do
-    get checkout_index_url
-    assert_response :success
+  test 'should redirect since dne' do
+    org = Organization.find_or_create_by!(org_id: 999_999) do |o|
+      o.org_name = 'Test Org'
+      o.org_location = 'Test Location'
+      o.public_access = true
+    end
+
+    get organization_checkout_url(organization_org_id: org.org_id, order_id: 1)
+    assert_response :redirect
   end
+
+  # TODO: add test for going to checkout page
 end
