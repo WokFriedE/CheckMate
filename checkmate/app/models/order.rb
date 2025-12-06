5fdc754dc948cf210fc37bcfee0d6b743055cb72
Allen <allenchackojohny@gmail.com>
2025-12-05 19:07:13 -0500

    order functions

--- checkmate/app/models/order.rb
+++ checkmate/app/models/order.rb
@@ -20,4 +20,157 @@ class Order < ApplicationRecord
   def self.complete_orders_info
     Order.includes(:user_datum, :order_details, :returns).all
   end
+
+  def self.user_all_orders(user_id)
+    joins(:order_details)
+      .where(orders: { user_id: user_id })
+      .order(order_details: :checkout_time)
+      .select(
+        'orders.order_id',
+        'orders.order_date',
+        'orders.return_status',
+        'orders.order_type',
+        'order_details.order_detail_id',
+        'order_details.item_id',
+        'order_details.due_date',
+        'order_details.owner_org_id',
+        'order_details.checkout_time',
+        'order_details.approval_status'
+      )
+  end
+
+  def self.user_past_orders(user_id)
+    joins(:order_details)
+      .where(orders: { user_id: user_id })
+      .where('order_details.order_date < NOW()')
+      .where('orders.return_status = TRUE OR orders.return_status IS NULL')
+      .order(order_details: :checkout_time)
+      .select(
+        'orders.order_id',
+        'orders.order_date',
+        'orders.return_status',
+        'orders.order_type',
+        'order_details.order_detail_id',
+        'order_details.item_id',
+        'order_details.due_date',
+        'order_details.owner_org_id',
+        'order_details.checkout_time',
+        'order_details.approval_status'
+      )
+  end
+
+  def self.user_current_orders(user_id)
+    joins(:order_details)
+      .where(orders: { user_id: user_id })
+      .where("orders.order_date < NOW()")
+      .where(return_status: false)
+      .order(order_details: :checkout_time)
+      .select(
+        'orders.order_id',
+        'orders.order_date',
+        'orders.return_status',
+        'orders.order_type',
+        'order_details.order_detail_id',
+        'order_details.item_id',
+        'order_details.due_date',
+        'order_details.owner_org_id',
+        'order_details.checkout_time',
+        'order_details.approval_status'
+      ) 
+  end
+
+  def self.user_future_orders(user_id)
+    joins(:order_details)
+      .where(orders: { user_id: user_id })
+      .where("order_details.checkout_time > ?", Time.current)
+      .order(order_details: :checkout_time)
+      .select(
+        'orders.order_id',
+        'orders.order_date',
+        'orders.return_status',
+        'orders.order_type',
+        'order_details.order_detail_id',
+        'order_details.item_id',
+        'order_details.due_date',
+        'order_details.owner_org_id',
+        'order_details.checkout_time',
+        'order_details.approval_status'
+      ) 
+  end
+
+  def self.org_all_orders(owner_org_id)
+    joins(:order_details)
+      .where(order_details: { owner_org_id: owner_org_id })
+      .order(order_details: :checkout_time)
+      .select(
+        'orders.order_id',
+        'orders.order_date',
+        'orders.return_status',
+        'orders.order_type',
+        'order_details.order_detail_id',
+        'order_details.item_id',
+        'order_details.due_date',
+        'order_details.owner_org_id',
+        'order_details.checkout_time',
+        'order_details.approval_status'
+      )
+  end
+
+  def self.org_past_orders(owner_org_id)
+    joins(:order_details)
+      .where(order_details: { owner_org_id: owner_org_id })
+      .where('order_details.order_date < NOW()')
+      .order(order_details: :checkout_time)
+      .select(
+        'orders.order_id',
+        'orders.order_date',
+        'orders.return_status',
+        'orders.order_type',
+        'order_details.order_detail_id',
+        'order_details.item_id',
+        'order_details.due_date',
+        'order_details.owner_org_id',
+        'order_details.checkout_time',
+        'order_details.approval_status'
+      )
+  end
+
+  def self.org_current_orders(owner_org_id)
+    joins(:order_details)
+      .where(order_details: { owner_org_id: owner_org_id })
+      .where("orders.order_date < NOW()")
+      .where(return_status: false)
+      .order(order_details: :checkout_time)
+      .select(
+        'orders.order_id',
+        'orders.order_date',
+        'orders.return_status',
+        'orders.order_type',
+        'order_details.order_detail_id',
+        'order_details.item_id',
+        'order_details.due_date',
+        'order_details.owner_org_id',
+        'order_details.checkout_time',
+        'order_details.approval_status'
+      ) 
+  end
+
+  def self.org_future_orders(owner_org_id)
+    joins(:order_details)
+      .where(order_details: { owner_org_id: owner_org_id })
+      .where("order_details.checkout_time > ?", Time.current)
+      .order(order_details: :checkout_time)
+      .select(
+        'orders.order_id',
+        'orders.order_date',
+        'orders.return_status',
+        'orders.order_type',
+        'order_details.order_detail_id',
+        'order_details.item_id',
+        'order_details.due_date',
+        'order_details.owner_org_id',
+        'order_details.checkout_time',
+        'order_details.approval_status'
+      ) 
+  end
 end
